9e0d3449d85caf8d42eb79b7ac47e00784f3f0ea
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2015-12-16 14:53:11 -0500

    Added new scene
    Movement in space

---
+++ SpaceBro/Assets/Scripts/Follow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+    // Use this for initialization
+
+    public GameObject go;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = go.transform.position - new Vector3(0, 0, 5); 
+        transform.rotation = go.transform.rotation;
+
+    }
+}
---
+++ SpaceBro/Assets/Scripts/MoveSpaceship.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveSpaceship : MonoBehaviour {
+
+    GameObject[] planets;
+    public float gravity;
+	// Use this for initialization
+	void Start () {
+        planets = GameObject.FindGameObjectsWithTag("Planet");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (GameObject go in planets)
+        {
+
+            gravity = (GetComponent<Rigidbody2D>().mass * go.GetComponent<Rigidbody2D>().mass) /
+                      ((go.transform.position - transform.position).sqrMagnitude);
+
+            GetComponent<Rigidbody2D>().AddForce((go.transform.position - transform.position).normalized * gravity);
+        }
+    }
+}
