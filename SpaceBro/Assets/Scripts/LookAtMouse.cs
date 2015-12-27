9199c18674d69cb0775803c29b07a4a0aaf04104
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2015-12-27 15:36:09 -0500

    Arms rotation, backward movement animation
    Also disabled particle, because the last unity update floods the console
    with errors. (When the number of particles is 0, the renderer goes
    crazy)

--- SpaceBro/Assets/Scripts/LookAtMouse.cs
+++ SpaceBro/Assets/Scripts/LookAtMouse.cs
@@ -2,19 +2,22 @@
 using System.Collections;
 
 public class LookAtMouse : MonoBehaviour {
-    public string rott;
-
-	// Use this for initialization
-	void Start () {
-	
+    public Vector3 Offset = Vector3.zero;
+    public MoveCharacter mc;
+    // Use this for initialization
+    void Start() {
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        Vector3 pos = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.parent.position);
+        Vector3 pos = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         Vector3 rot = new Vector3(0, 0, Mathf.Atan2(pos.x, pos.y) * Mathf.Rad2Deg);
-        rott = rot.ToString();
 
-        transform.rotation = Quaternion.Euler(rot);
+        // If the mc script is set, then this parts copies the orientation of the linked script.
+        Direction dir = Direction.RIGHT;
+        if (mc != null)
+            dir = mc.LookDirection;
+
+        transform.rotation = Quaternion.Euler(- (int)dir * rot + Offset + transform.parent.parent.rotation.eulerAngles);
 	}
 }
--- SpaceBro/Assets/Scripts/MoveCharacter.cs
+++ SpaceBro/Assets/Scripts/MoveCharacter.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
-
+public enum Direction { LEFT = -1, RIGHT = 1 }
 public class MoveCharacter : MonoBehaviour
 {
+
     const float INITIAL_DRAG = 20;
-    const float FALLING_DRAG = 1;
+    const float FALLING_DRAG = 0.1f;
+    const int JUMP_COOLDOWN = 20;
 
     GameObject[] Planets;
     Rigidbody2D Rb { get; set; }
     Animator Anim { get; set; }
     bool isWalking { get; set; }
     bool isJumping { get; set; }
+    int jumpTimer = 0;
+
+    public Direction LookDirection { get; private set; }
+    Direction moveDirection { get; set; }
 
     // Use this for initialization
     void Start()
@@ -23,6 +29,11 @@ public class MoveCharacter : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        SetDirection();
+
+        if (jumpTimer >= 0)
+            jumpTimer -= 1;
+
         if (Rb.IsTouchingLayers())
         {
             Rb.drag = INITIAL_DRAG;
@@ -33,7 +44,7 @@ public class MoveCharacter : MonoBehaviour
             Rb.drag = FALLING_DRAG;
 
         }
-        HandleInput(); 
+        HandleInput();
 
         Quaternion rot = Quaternion.Euler(new Vector3(0, 0, GetAngle(transform.position - FindClosestPlanet().transform.position) - 90));
         transform.rotation = rot;
@@ -44,39 +55,64 @@ public class MoveCharacter : MonoBehaviour
     void HandleInput()
     {
         isWalking = false;
+        if (Input.GetKey(KeyCode.W))
+            Jump();
 
-        if (Input.GetKey(KeyCode.W) && !isJumping)
+        if (Input.GetKey(KeyCode.D))
+            Move(Direction.RIGHT);
+
+        if (Input.GetKey(KeyCode.A))
+            Move(Direction.LEFT);
+    }
+
+    void Flip(Direction dir)
+    {
+        LookDirection = dir;
+        Vector3 scale = transform.localScale;
+        scale.x = (int)dir * Mathf.Abs(scale.x);
+        transform.localScale = scale;
+    }
+
+    void Move(Direction dir)
+    {
+        moveDirection = dir;
+
+        if (Rb.velocity.magnitude < 5)
+        {
+            Rb.AddForce((int)dir * transform.right * Rb.drag * 3f);
+            isWalking = true;
+        }
+    }
+
+    void Jump()
+    {
+        if (!isJumping && jumpTimer <= 0)
         {
             isJumping = true;
             Rb.AddForce(transform.up * 140);
-            Rb.drag = 1;
-
+            Rb.drag = FALLING_DRAG;
+            jumpTimer = JUMP_COOLDOWN;
         }
+    }
 
-        if (Input.GetKey(KeyCode.D) && Rb.velocity.magnitude < 5)
-        {
-
-            Vector3 theScale = transform.localScale;
-            theScale.x = Mathf.Abs(theScale.x);
-            transform.localScale = theScale;
-            Rb.AddForce(transform.right * Rb.drag * 3f);
-            isWalking = true;
-        }
-        if (Input.GetKey(KeyCode.A) && Rb.velocity.magnitude < 5)
-        {
-
-            Vector3 theScale = transform.localScale;
-            theScale.x = -Mathf.Abs(theScale.x);
-            transform.localScale = theScale;
-            Rb.AddForce(transform.right * Rb.drag * -3f);
-            isWalking = true;
-        }
-
+    void SetDirection()
+    {
+        if (Input.mousePosition.x < Camera.main.WorldToScreenPoint(transform.position).x)
+            Flip(Direction.LEFT);
+        else
+            Flip(Direction.RIGHT);
     }
 
     void SetAnimationParameters()
     {
         Anim.SetBool("IsWalking", isWalking);
+
+        // If the direction the character is moving equals the direction it is looking, then the animation plays normally. Otherwise, it plays backwards.
+        //print(moveDirection.ToString());
+
+        //print(LookDirection);
+
+        Anim.SetFloat("SpeedMultiplier", moveDirection == LookDirection ? 1 : -1);
     }
 
     GameObject FindClosestPlanet()
@@ -96,7 +132,12 @@ public class MoveCharacter : MonoBehaviour
     {
         return Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
     }
-    
+
+    float GetAngle(Vector3 v1, Vector3 v2)
+    {
+        return Mathf.Acos(Vector3.Dot(v1.normalized, v2.normalized));
+    }
+
     //Computes the distance between the current gameobject and a given gameobject
     float ComputeDist(GameObject gameObject)
     {
